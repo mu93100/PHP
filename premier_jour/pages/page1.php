886179cd5b17911d96a209748ec196e6b2378c50
muriel <murielehlinger@gmail.com>
2025-04-14 02:09:11 +0200

    jjj

--- premier_jour/pages/page1.php
+++ premier_jour/pages/page1.php
@@ -5,6 +5,7 @@ include_once "../component/header.php";
 
 $variable = 123;
 
+
 ?>
 
 
--- premier_jour/pages/page2.php
+++ premier_jour/pages/page2.php
@@ -9,8 +9,10 @@ include_once "../component/header.php"
 
 
 <h1>page2</h1>
+<div class="bisque">D I V </div>
 
 
 <?php 
 
 include_once "../component/footer.php";
+?>
