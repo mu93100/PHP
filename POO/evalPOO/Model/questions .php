c7acf88ee47fcdb745ea6dda5cea2640154f7801
mu93100 <muriel.ehlinger@lepoles.org>
2025-04-29 16:18:12 +0200

    eval29avril22

---
+++ POO/evalPOO/Model/questions .php
@@ -0,0 +1,20 @@
+<!-- 
+VI
+une classe est la structure de fonctionnement qui va pouvoir donner un objet.
+l'objet est une instanciation de la class ( nouvelle version de la classe)  
+
+VII
+public : les propriétés ou méthodes seront utilisables directement en dehors de la classe
+private : on ne peut pas les appeler depuis l'extérieur de la classe.
+elles peuvent sortir de la classe/être exprimése via une autre méthode
+VIII
+une méthode native de PHP dont le nom commence par  2 underscores
+ex : __construct
+IX
+pour convertir l’objet en string
+
+X
+avec le mot-clé const et le nom en majuscule
+ex : public const MACONSTANTE = "valeur de ma constante";
+
+-->
